d3215e9490da46bbfeab677cbe7ff6408fe1fb31
Mariano Giagante <mariano.giagante@gmail.com>
2017-07-20 06:37:59 -0300

    Implements guessing functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand(100)
+user_won = false
 
 get '/' do
-  erb :index, :locals => {:number => secret_number}
+  if params['guess'].to_i > secret_number
+    message = "Too high!"
+  elsif params['guess'].to_i < secret_number
+    message = "Too low!"
+  else
+    message = "You got it right!"
+    user_won = true
+  end
+  erb :index, :locals => {:number => secret_number, :message => message, :user_won => user_won}
 end
