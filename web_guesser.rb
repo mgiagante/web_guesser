0f41dd55992ccce11cb063156c645aaf0c2f521e
Mariano Giagante <mariano.giagante@gmail.com>
2017-07-29 22:23:35 -0300

    Implements guess limit and losing condition; Adds bundler require statement

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,22 @@
+require 'bundler'
+Bundler.require
+
 require 'sinatra'
 require 'sinatra/reloader'
 
 @@remaining_tries = 5
 
-set :number, rand(100)
+@@number = rand(100)
 
 helpers do
   def check_guess(guess)
     if guess
-      if guess.to_i > settings.number
-        message = "Too high!"
-      elsif guess.to_i < settings.number
-        message = "Too low!"
+      if guess.to_i > @@number 
+        { message: "Too high!", guessed_right: false }
+      elsif guess.to_i < @@number
+        { message: "Too low!", guessed_right: false }
       else
-        message = "You got it right!"
+        { message: "You got it right!", guessed_right: true }
       end
     else
       nil
@@ -31,22 +34,37 @@ helpers do
     end
   end
 
-  def guessed_right?
-    @closeness.downcase.include? 'right'
+  def won?
+    check_guess(@guess)[:guessed_right]
+  end
+
+  def lost?
+    @@remaining_tries.zero?
   end
 end
 
 get '/' do
-  guess = params["guess"]
-  message = check_guess(guess)
+  puts "#{@@number} generated!"
+  @guess = params["guess"]
+  message = check_guess(@guess) ? check_guess(@guess)[:message] : nil
 
-  if guess
-    @closeness = closeness_for(guess.to_i, settings.number)
-    @@remaining_tries -= 1 unless guessed_right? 
+  if @guess
+    @closeness = closeness_for(@guess.to_i, @@number)
+
+    if won? || lost? 
+      @@number = rand(100)
+      @@remaining_tries = 5
+    else
+      @@remaining_tries -= 1
+    end
+
+    if lost? 
+      message = "You lost. A new secret number has been generated!"
+    end
   end
 
   erb :index, :locals => {
-    :number => settings.number, 
+    :number => @@number, 
     :message => message, 
     :closeness => @closeness
   }
